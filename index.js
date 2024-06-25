1098066b81fc84c1f9dd6d6b5c88b9073e00b634
explorer1500 <163674076+explorer7733@users.noreply.github.com>
2024-06-24 18:19:45 -0700

    add CONCAT at index.js, replaced teh screenshot

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ const viewAllRoles = () => {
 // Function to view all employees
 const viewAllEmployees = () => {
    
-    const query = `SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name, manager.first_name  AS manager_first_name, manager.last_name AS manager_last_name FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id
+    const query = `SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name AS department, CONCAT(manager.first_name, ' ', manager.last_name) AS manager  FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id
     LEFT JOIN employee AS manager ON employee.manager_id = manager.id`;
     pool.query(query, (err, res) => {
         if (err) throw err;
